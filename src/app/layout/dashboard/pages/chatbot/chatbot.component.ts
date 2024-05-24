9f7f52cceb89d91076be396bb272dccd8d58b1ca
Ovejero92 <ovejero.gustavo92@gmail.com>
2024-05-24 02:40:47 -0300

    entrega final

--- src/app/layout/dashboard/pages/chatbot/chatbot.component.ts
+++ src/app/layout/dashboard/pages/chatbot/chatbot.component.ts
@@ -6,15 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './chatbot.component.scss'
 })
 export class ChatbotComponent{
-  messages: string[] = [];
+  messages: any[] = [];
   userMessage: string = '';
   typingMessage: string = '';
+  options: string[] = [];
 
   sendMessage() {
     if (this.userMessage.trim() !== '') {
-      this.messages.push(this.userMessage);
+      this.messages.push({ text: this.userMessage, type: 'user' });
       this.userMessage = '';
-      this.simulateTyping('This is a simulated typing response from the chatbot.');
+      this.simulateTyping('Hola Mi nombre es india mi trabajo es enviar tu error de cualquiera de los servicios.\n¿Cual de ellos te esta fallando? ⬇clickealo⬇')
+      setTimeout(()=>{
+        this.provideOptions(['Profesores', 'Alumnos', 'Tecnologias', 'Aulas']);
+      },13200);
     }
   }
 
@@ -28,9 +32,20 @@ export class ChatbotComponent{
 
       if (index >= message.length) {
         clearInterval(typingInterval);
-        this.messages.push(this.typingMessage);
+        this.messages.push({ text: this.typingMessage, type: 'bot' });
         this.typingMessage = '';
       }
     }, 100); // Ajusta el intervalo para cambiar la velocidad de escritura
+  }
+
+  provideOptions(options: string[]) {
+    this.options = options;
+  }
+
+  selectOption(option: string) {
+    this.messages.push({ text: option, type: 'user' });
+    this.options = [];
+    this.simulateTyping(`Has seleccionado ${option}.En estos momentos mande la orden para que analizen donde se encuenta el error en breve te mandare cuando lo solucionemos \ngracias por avisarnos...`);
+    // Aquí puedes agregar lógica para cambiar las preguntas basadas en la opción seleccionada
   }
 }
