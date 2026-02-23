93db546a84d720435bbe487b90bb69c15b40fb84
GusDev <ovejero.gustavo92@gmail.com>
2026-02-23 13:38:47 -0300

    vercel2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://pf-ovejero-backend.onrender.com',
-  baseAPIURL: 'https://pf-ovejero-backend.onrender.com'
+  apiUrl: 'https://pf-ovejero.onrender.com',
+  baseAPIURL: 'https://pf-ovejero.onrender.com'
 };
